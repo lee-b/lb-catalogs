eb6179d7640d16cd45ab15cf5eecccfc89a88946
Lee Braiden <lee.b@kintassa.com>
2011-05-03 14:32:05 +0100

    include Kintassa_Core based on function presence; don't rely on wordpress plugin loaded checking.

--- kintassa_catalog.php
+++ kintassa_catalog.php
@@ -10,74 +10,75 @@ Copyright: Copyright (c) 2011 Kintassa.
 License: All rights reserved.  Contact Kintassa for licensing.
 */
 
+
 if (!function_exists('kintassa_core')) {
-	echo "This plugin requires Kintassa Core";
-} else {
-	function kin_cat_inc($subpath) {
-		$full_subpath = "src/" . $subpath;
-		if (DIRECTORY_SEPARATOR == "/") {
-			$real_subpath = $full_subpath;
-		} else {
-			$real_subpath = str_replace("/", DIRECTORY_SEPARATOR, $full_subpath);
-		}
-		return dirname(__file__) . DIRECTORY_SEPARATOR . $real_subpath;
+	require_once ( WP_PLUGIN_DIR . '/Kintassa_Core/kintassa_core.php' );
+}
+
+function kin_cat_inc($subpath) {
+	$full_subpath = "src/" . $subpath;
+	if (DIRECTORY_SEPARATOR == "/") {
+		$real_subpath = $full_subpath;
+	} else {
+		$real_subpath = str_replace("/", DIRECTORY_SEPARATOR, $full_subpath);
 	}
-	
-	require_once(kintassa_core('kin_wp_plugin.php'));
-	require_once(kin_cat_inc('kcat_config.php'));
-	require_once(kin_cat_inc('kcat_db.php'));
-	require_once(kin_cat_inc('kcat_menu.php'));
-	require_once(kin_cat_inc('kcat_shortcode.php'));
-	require_once(kin_cat_inc('kcat_db.php'));
-	require_once(kin_cat_inc('kcat_tags.php'));
-	
-	class KintassaCatalogPlugin extends KintassaWPPlugin {
-		function __construct() {
-			parent::__construct(__file__);
-	
-			$kintassa_catalogs_menu = new KintassaCatalogMenu();
-			$kintassa_catalogs_shortcode = new KintassaCatalogShortcode();
-	
-			add_action('init', array($this, 'install_scripts'));
-	
-			$write_dirs = array(KCAT_UPLOAD_PATH, KCAT_CACHE_PATH);
-			foreach ($write_dirs as $wd) {
-				if (!file_exists($wd)) {
-					exit("ERROR: '$wd' does not exist!");
-				}
-	
-				if (!is_writeable($wd)) {
-					exit("ERROR: '$wd' is not writeable!");
-				}
+	return dirname(__file__) . DIRECTORY_SEPARATOR . $real_subpath;
+}
+
+require_once(kintassa_core('kin_wp_plugin.php'));
+require_once(kin_cat_inc('kcat_config.php'));
+require_once(kin_cat_inc('kcat_db.php'));
+require_once(kin_cat_inc('kcat_menu.php'));
+require_once(kin_cat_inc('kcat_shortcode.php'));
+require_once(kin_cat_inc('kcat_db.php'));
+require_once(kin_cat_inc('kcat_tags.php'));
+
+class KintassaCatalogPlugin extends KintassaWPPlugin {
+	function __construct() {
+		parent::__construct(__file__);
+
+		$kintassa_catalogs_menu = new KintassaCatalogMenu();
+		$kintassa_catalogs_shortcode = new KintassaCatalogShortcode();
+
+		add_action('init', array($this, 'install_scripts'));
+
+		$write_dirs = array(KCAT_UPLOAD_PATH, KCAT_CACHE_PATH);
+		foreach ($write_dirs as $wd) {
+			if (!file_exists($wd)) {
+				exit("ERROR: '$wd' does not exist!");
 			}
-		}
-	
-		function reg_script($name, $relpath) {
-			$abs_url = plugins_url("scripts" . DIRECTORY_SEPARATOR . $relpath, __file__);
-			wp_register_script($name, $abs_url, false, null);
-	        $myStyleUrl = plugins_url('/stylesheets/kintassa_catalog.css', __file__);
-		    wp_register_style('kintassa_catalog', $myStyleUrl);
-		}
-	
-		function install_scripts() {
-			$this->reg_script("jquery_cycle", "jquery.cycle.all.min.js");
-	
-			wp_enqueue_script("jquery");
-			wp_enqueue_script("jquery_cycle");
-	        wp_enqueue_style('kintassa_catalog');
-		}
-	
-		function install() {
-			kintassa_catalogs_setup_db();
-		}
-	
-		function remove() {
+
+			if (!is_writeable($wd)) {
+				exit("ERROR: '$wd' is not writeable!");
+			}
 		}
 	}
 
-	// instanciate the plugin
-	$kCatalogPlugin = new KintassaCatalogPlugin();
-	$kCatalogPlugin->install();
+	function reg_script($name, $relpath) {
+		$abs_url = plugins_url("scripts" . DIRECTORY_SEPARATOR . $relpath, __file__);
+		wp_register_script($name, $abs_url, false, null);
+        $myStyleUrl = plugins_url('/stylesheets/kintassa_catalog.css', __file__);
+	    wp_register_style('kintassa_catalog', $myStyleUrl);
+	}
+
+	function install_scripts() {
+		$this->reg_script("jquery_cycle", "jquery.cycle.all.min.js");
+
+		wp_enqueue_script("jquery");
+		wp_enqueue_script("jquery_cycle");
+        wp_enqueue_style('kintassa_catalog');
+	}
+
+	function install() {
+		kintassa_catalogs_setup_db();
+	}
+
+	function remove() {
+	}
 }
+
+// instanciate the plugin
+$kCatalogPlugin = new KintassaCatalogPlugin();
+$kCatalogPlugin->install();
 
 ?>
