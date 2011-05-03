e3a2af7574305cffc58532fc24830a9478e4c26e
Lee Braiden <lee.b@kintassa.com>
2011-05-03 11:37:36 +0100

    Remove applets that don't apply to this plugin

--- applets/applet_invalid.php
+++ applets/applet_invalid.php
@@ -7,15 +7,15 @@ License: All rights reserved.  Contact Kintassa should you wish to license this 
 */
 
 $_PLUGIN_ROOT = dirname(dirname(__file__));
-require_once($_PLUGIN_ROOT . DIRECTORY_SEPARATOR . "src/kgal_gallery_applet.php");
+require_once($_PLUGIN_ROOT . DIRECTORY_SEPARATOR . "src/kcat_catalog_applet.php");
 
 /***
  * Dummy renderer used for error messages when the requested renderer
  * doesn't exist
  */
-class KintassaInvalidGalleryApplet extends KintassaGalleryApplet {
+class KintassaInvalidCatalogApplet extends KintassaCatalogApplet {
 	static function register() {
-		KintassaGalleryApplet::register('KintassaInvalidGalleryApplet', 'invalid', null);
+		KintassaCatalogApplet::register('KintassaInvalidCatalogApplet', 'invalid', null);
 	}
 
 	function classes() {
@@ -27,20 +27,20 @@ class KintassaInvalidGalleryApplet extends KintassaGalleryApplet {
 	function render() {
 		$applet = $this;
 
-		$gallery = $this->gallery;
+		$catalog = $this->catalog;
 
 		$unique_id = $this->unique_id();
 
 		$cls = $this->classes_attrib_str();
 		$sty = $this->styles_attrib_str();
 
-		$not_avail_msg = __("This gallery cannot be displayed. Please check the gallery ID exists, (re)install the necessary GalleryApplets for its display method, or change the display method to one that's currently available.");
+		$not_avail_msg = __("This catalog cannot be displayed. Please check the catalog ID exists, (re)install the necessary CatalogApplets for its display method, or change the display method to one that's currently available.");
 
 		$template = $this->template_path("invalid", "render");
 		require($template);
 	}
 }
 
-KintassaInvalidGalleryApplet::register();
+KintassaInvalidCatalogApplet::register();
 
 ?>
